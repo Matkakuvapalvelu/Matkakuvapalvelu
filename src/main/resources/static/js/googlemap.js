2be64dbc452be011f35ead1f3ab4c4a677a5b55b
Valtis <erkka.kaaria@helsinki.fi>
2014-11-14 13:52:03 +0200

    Fixed few css paths as they were broken. Added google map to post view that uses image longitude\latitude

---
+++ src/main/resources/static/js/googlemap.js
@@ -0,0 +1,33 @@
+
+var infoWindow  = new google.maps.InfoWindow({
+    content:"You were here!",
+    maxWidth: 300,
+    maxHeight: 300,
+    width: 150,
+    height: 150
+});
+
+function initialize(latitude, longitude) {
+
+    var mapOptions = {
+        center: { lat: latitude, lng: longitude},
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('googleMap'),mapOptions);
+    drawNewMarker(map, latitude, longitude)
+}
+
+
+function drawNewMarker(map, lat, lng){
+    var marker = new google.maps.Marker({
+        position: { lat: lat, lng: lng}
+    });
+    google.maps.event.addListener(marker, 'click', onClickMarker);
+    marker.setMap(map);
+}
+
+var onClickMarker = function(event) {
+    infoWindow.open(this.map, this);
+}/**
+ * Created by Omistaja on 14.11.2014.
+ */
