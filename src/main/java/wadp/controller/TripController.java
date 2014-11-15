d477ec573e26a4769370529869e17a78d38b2fa8
Valtis <erkka.kaaria@helsinki.fi>
2014-11-16 00:51:44 +0200

    Added transactional tag to controller method in an attempt to fix postgresql errors

--- src/main/java/wadp/controller/TripController.java
+++ src/main/java/wadp/controller/TripController.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +37,7 @@ public class TripController {
     }
     
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    @Transactional // postgresql REALLY does not like it if you fetch large fields (like images) outside transactions
     public String viewSingleTrip(Model model, @PathVariable("id") Long id){
         List<Post> posts = new ArrayList<>();        
         List<double[]> coordinates = new ArrayList<>();
